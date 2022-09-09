cef9fb4e1de2b2c69a81e3b68a474be53d6a1e92
JP <jpsiyyadri@gmail.com>
2022-09-09 14:08:46 +0530

    UPD: deploy script

--- scripts/copy.sh
+++
@@ -1,2 +0,0 @@
-scp -i "/Users/jaiprakash/.jenkins/workspace/aws_keys/my-aws-server-1-key-pair.pem" -r /Users/jaiprakash/.jenkins/workspace/demo-mbp-django-app_main ec2-user@ec2-15-206-91-233.ap-south-1.compute.amazonaws.com:/home/ec2-user/apps
-# scp -i "/Volumes/My Drive/codebases/projects/secrets/DevthJestAWS/my-aws-server-1-key-pair.pem" -r demo_app/ ec2-user@ec2-15-206-91-233.ap-south-1.compute.amazonaws.com:/home/ec2-user/killme
